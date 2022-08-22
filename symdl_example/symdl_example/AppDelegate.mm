a7a9c00492f4758bf5b9b271e39ac5aacb03a34a
iainchen <iainchen@tencent.com>
2022-08-22 12:49:18 +0800

    performance test

--- symdl_example/symdl_example/AppDelegate.mm
+++ symdl_example/symdl_example/AppDelegate.mm
@@ -23,13 +23,29 @@ extern "C" void my_load(void *p){
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    printf("fopen addrss: %p\n", fopen);
-    printf("dymdl fopen addrss: %p\n", symdl("fopen"));
+    
+    
+    struct timespec time1 = {0, 0};
+    struct timespec time2 = {0, 0};
+    struct timespec time3 = {0, 0};
+    clock_gettime(CLOCK_MONOTONIC, &time1);
+    void *my_load_ptr = symdl("my_load");
+    clock_gettime(CLOCK_MONOTONIC, &time2);
+    void *fopen_ptr = symdl("fopen");
+    clock_gettime(CLOCK_MONOTONIC, &time3);
+    
+    uint64_t d1 = (time2.tv_sec * 1000000 + time2.tv_nsec/1000) - (time1.tv_sec * 1000000 + time1.tv_nsec/1000);
+    uint64_t d2 = (time3.tv_sec * 1000000 + time3.tv_nsec/1000) - (time2.tv_sec * 1000000 + time2.tv_nsec/1000);
+    
+    printf("d1: %llu, d1: %llu \n", d1, d2);
     
     
     printf("my_load addrss: %p\n", my_load);
-    printf("dymdl fopmy_loaden addrss: %p\n", symdl("my_load"));
+    printf("dymdl fopmy_loaden addrss: %p\n", my_load_ptr);
+    
+    printf("fopen addrss: %p\n", fopen);
+    printf("dymdl fopen addrss: %p\n", fopen_ptr);
+    
     
     // Override point for customization after application launch.
     return YES;
